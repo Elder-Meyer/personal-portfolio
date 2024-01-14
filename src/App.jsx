993cc1e86d6b6d5f2fc5f012ab7c6c13e47d57af
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-01-14 05:14:30 -0600

    1.3.5 Add set up the app to work with RGA / JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { ThemeProvider } from "@mui/material"
 import { Router } from "./routes/Router"
 import { lightTheme } from "./styles/ThemeMui"
+import ReactGA from 'react-ga';
+const TRACKING_ID = "6395311067"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID, {debug: true});
 
 function App() {
   return (
