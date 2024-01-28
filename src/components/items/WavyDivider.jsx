864a693930f386aea4b262a79b21bae427433dc1
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-01-28 13:02:18 -0600

    1.7.1 Update Select Color WavyDivider

--- src/components/items/WavyDivider.jsx
+++ src/components/items/WavyDivider.jsx
@@ -7,9 +7,7 @@ export const WavyDivider = ({invertColors}) => {
 
     const location = useLocation();
     const isHome = location.pathname === "/home";
-
     const theme = useTheme()
-
     const styles = `
         .editorial { 
             display: block;
@@ -18,64 +16,62 @@ export const WavyDivider = ({invertColors}) => {
             max-height: 60px;
             margin: 0;
             z-index:5;
-            
-            
         }
         
         .parallax1 > use {
             animation: move-forever1 10s linear infinite;
             &:nth-child(1) {
-            animation-delay: -2s;
+                animation-delay: -2s;
             }
         }
         .parallax2 > use {
             animation: move-forever2 8s linear infinite;
             &:nth-child(1) {
-            animation-delay: -2s;
+                animation-delay: -2s;
             }
         }
         .parallax3 > use {
             animation: move-forever3 6s linear infinite;
             &:nth-child(1) {
-            animation-delay: -2s;
+                animation-delay: -2s;
             }
         }
         .parallax4 > use {
             animation: move-forever4 4s linear infinite;
             &:nth-child(1) {
-            animation-delay: -2s;
+                animation-delay: -2s;
             }
         }
         @keyframes move-forever1 {
             0% {
-            transform: translate(85px, 0%);
+                transform: translate(85px, 0%);
             }
             100% {
-            transform: translate(-90px, 0%);
+                transform: translate(-90px, 0%);
             }
         }
         @keyframes move-forever2 {
             0% {
-            transform: translate(-90px, 0%);
+                transform: translate(-90px, 0%);
             }
             100% {
-            transform: translate(85px, 0%);
+                transform: translate(85px, 0%);
             }
         }
         @keyframes move-forever3 {
             0% {
-            transform: translate(85px, 0%);
+                transform: translate(85px, 0%);
             }
             100% {
-            transform: translate(-90px, 0%);
+                transform: translate(-90px, 0%);
             }
         }
         @keyframes move-forever4 {
             0% {
-            transform: translate(-90px, 0%);
+                transform: translate(-90px, 0%);
             }
             100% {
-            transform: translate(85px, 0%);
+                transform: translate(85px, 0%);
             }
         }
     `
@@ -100,10 +96,10 @@ export const WavyDivider = ({invertColors}) => {
                 />
             </defs>
             <g className="parallax1">
-                <use xlinkHref="#gentle-wave" x="50" y="3" fill={invertColors ? theme.palette.primary.main : theme.palette.text.secondary}/>
+                <use xlinkHref="#gentle-wave" x="50" y="3" fill={invertColors ? theme.palette.primary.main : theme.palette.secondary.dark}/>
             </g>
             <g className="parallax2">
-                <use xlinkHref="#gentle-wave" x="50" y="0" fill={invertColors ? theme.palette.primary.light : theme.palette.text.secondary}/>
+                <use xlinkHref="#gentle-wave" x="50" y="0" fill={invertColors ? theme.palette.primary.light : theme.palette.secondary.light}/>
             </g>
             <g className="parallax3">
                 <use xlinkHref="#gentle-wave" x="50" y="9" fill={invertColors ? theme.palette.text.disabled : theme.palette.primary.light}/>
