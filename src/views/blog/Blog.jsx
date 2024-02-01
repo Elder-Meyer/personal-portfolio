cad924246c550ab5d7b6a7ca26470c85507d7932
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-01 03:23:54 -0600

    2.2.02 Update Blog View Basic

--- src/views/blog/Blog.jsx
+++ src/views/blog/Blog.jsx
@@ -1,14 +1,22 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, Container, Grid } from '@mui/material'
 import React from 'react'
 import { WorkingOnThat } from '../../components/items/WorkingOnThat'
 
 export const Blog = () => {
   return (
-    <Box sx={{backgroundColor: "background.paper"}}>
-      <Typography color="text.secondary" variant='h2' >
-          Blog
-      </Typography>  
-      <WorkingOnThat />
+    <Box sx={{backgroundColor: "background.default"}}>
+      <Container maxWidth="lg">
+        <Grid container columnSpacing={2} py={5}>
+          <Grid item xs={12}>
+            <Typography color="text.secondary" variant='subtitle1' textAlign="center">
+                Blog
+            </Typography>  
+          </Grid>
+          <Grid item xs={12}>
+            <WorkingOnThat />
+          </Grid>
+        </Grid>
+      </Container>
     </Box>
   )
 }
