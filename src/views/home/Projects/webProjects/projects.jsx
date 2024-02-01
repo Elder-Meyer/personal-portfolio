1dabf7443752d37de7a18fe890b7baf3b93f73a1
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-01 00:26:07 -0600

    2.1.8 Update Img's Scale Viewport Projects

--- src/views/home/Projects/webProjects/projects.jsx
+++ src/views/home/Projects/webProjects/projects.jsx
@@ -1,68 +1,92 @@
-// IMAGENES PRINCIPALES
-import pro_pri_1 from "../../../../assets/img/img-projects/pro_pri_1.jpg";
-import pro_pri_2 from "../../../../assets/img/img-projects/pro_pri_2.jpg";
-import pro_pri_3 from "../../../../assets/img/img-projects/pro_pri_3.jpg";
-import pro_pri_5 from "../../../../assets/img/img-projects/pro_pri_5.jpg";
-import pro_pri_6 from "../../../../assets/img/img-projects/pro_pri_6.jpg";
-import pro_pri_7 from "../../../../assets/img/img-projects/pro_pri_7.jpg";
-
-// IMAGENES GRANDES
-import pro_pri_1_md from "../../../../assets/img/img-projects/pro_pri_1_md.jpg";
-import pro_pri_2_md from "../../../../assets/img/img-projects/pro_pri_2_md.jpg";
-import pro_pri_3_md from "../../../../assets/img/img-projects/pro_pri_3_md.jpg";
-import pro_pri_5_md from "../../../../assets/img/img-projects/pro_pri_5_md.jpg";
-
-// IMAGENES MEDIANAS
-import pro_pri_1_sm from "../../../../assets/img/img-projects/pro_pri_1_sm.jpg";
-import pro_pri_2_sm from "../../../../assets/img/img-projects/pro_pri_2_sm.jpg";
-import pro_pri_3_sm from "../../../../assets/img/img-projects/pro_pri_3_sm.jpg";
-import pro_pri_5_sm from "../../../../assets/img/img-projects/pro_pri_5_sm.jpg";
-
-// IMAGENES PEQUEÑAS
-import pro_pri_1_xs from "../../../../assets/img/img-projects/pro_pri_1_xs.jpg";
-import pro_pri_2_xs from "../../../../assets/img/img-projects/pro_pri_2_xs.jpg";
-import pro_pri_3_xs from "../../../../assets/img/img-projects/pro_pri_3_xs.jpg";
-import pro_pri_5_xs from "../../../../assets/img/img-projects/pro_pri_5_xs.jpg";
+// imagenes
+// project 01
+import pri_proj_view_01 from '../../../../assets/img/img-projects/pri_proj_view_01.png'
+import proj_01_xl from '../../../../assets/img/img-projects/proj_01_xl.png'
+import proj_01_lg from '../../../../assets/img/img-projects/proj_01_lg.png'
+import proj_01_md from '../../../../assets/img/img-projects/proj_01_md.png'
+import proj_01_sm from '../../../../assets/img/img-projects/proj_01_sm.png'
+import proj_01_xs from '../../../../assets/img/img-projects/proj_01_xs.png'
+// project 02
+import pri_proj_view_02 from '../../../../assets/img/img-projects/pri_proj_view_02.png'
+import proj_02_xl from '../../../../assets/img/img-projects/proj_02_xl.png'
+import proj_02_lg from '../../../../assets/img/img-projects/proj_02_lg.png'
+import proj_02_md from '../../../../assets/img/img-projects/proj_02_md.png'
+import proj_02_sm from '../../../../assets/img/img-projects/proj_02_sm.png'
+import proj_02_xs from '../../../../assets/img/img-projects/proj_02_xs.png'
+// project 03
+import pri_proj_view_03 from '../../../../assets/img/img-projects/pri_proj_view_03.png'
+import proj_03_xl from '../../../../assets/img/img-projects/proj_03_xl.png'
+import proj_03_lg from '../../../../assets/img/img-projects/proj_03_lg.png'
+import proj_03_md from '../../../../assets/img/img-projects/proj_03_md.png'
+import proj_03_sm from '../../../../assets/img/img-projects/proj_03_sm.png'
+import proj_03_xs from '../../../../assets/img/img-projects/proj_03_xs.png'
+// project 04
+import pri_proj_view_04 from '../../../../assets/img/img-projects/pri_proj_view_04.png'
+import proj_04_xl from '../../../../assets/img/img-projects/proj_04_xl.png'
+import proj_04_lg from '../../../../assets/img/img-projects/proj_04_lg.png'
+import proj_04_md from '../../../../assets/img/img-projects/proj_04_md.png'
+import proj_04_sm from '../../../../assets/img/img-projects/proj_04_sm.png'
+import proj_04_xs from '../../../../assets/img/img-projects/proj_04_xs.png'
 
 export const projects = [
     {
       id: "project-01-corazon-huasteco",
       title: "Corazón Huasteco",
-      description: "Web application of culture.",
-      imgPrincipal: pro_pri_2,
-      imgMd: pro_pri_2_md,
-      imgSm: pro_pri_2_sm,
-      imgXs: pro_pri_2_xs,
+      date: "",
+      techs: ["", "", "", "", ""],
+      shortDescription: "Web application of culture.",
+      description: "this is a large description lorem lorem lorem lorem",
+      imgPrincipal: pri_proj_view_01,
+      imgXl: proj_01_xl,
+      imgLg: proj_01_lg,
+      imgMd: proj_01_md,
+      imgSm: proj_01_sm,
+      imgXs: proj_01_xs,
       toURL: "https://corazon-huasteco.com"
     },
     {
       id: "project-02-app-caih",
       title: "App CAIH",
-      description: "Web system for architects,",
-      imgPrincipal: pro_pri_3,
-      imgMd: pro_pri_3_md,
-      imgSm: pro_pri_3_sm,
-      imgXs: pro_pri_3_xs,
+      date: "",
+      techs: ["", "", "", "", ""],
+      shortDescription: "Web system for architects,",
+      description: "this is a large description lorem lorem lorem lorem",
+      imgPrincipal: pri_proj_view_02,
+      imgXl: proj_02_xl,
+      imgLg: proj_02_lg,
+      imgMd: proj_02_md,
+      imgSm: proj_02_sm,
+      imgXs: proj_02_xs,
       toURL: "https://huasteca-mx.web.app"
     },
     {
       id: "project-03-dulce-delicia",
       title: "Dulce Delicia",
-      description: "A simple movie search engine",
-      imgPrincipal: pro_pri_6,
-      imgMd: pro_pri_6, //revisar esto
-      imgSm: pro_pri_6, //revisar esto
-      imgXs: pro_pri_6, //revisar esto
-      toURL: "", //revisar esto
+      date: "",
+      techs: ["", "", "", "", ""],
+      shortDescription: "A candy store",
+      description: "this is a large description lorem lorem lorem lorem",
+      imgPrincipal: pri_proj_view_03,
+      imgXl: proj_03_xl,
+      imgLg: proj_03_lg,
+      imgMd: proj_03_md,
+      imgSm: proj_03_sm,
+      imgXs: proj_03_xs,
+      toURL: "https://dulce-delicia-b3afa.web.app/inicio", 
     },
     {
       id: "project-04-fashion-fusion",
       title: "Fashion Fusion",
-      description: "A store of clothes and accessories",
-      imgPrincipal: pro_pri_1,
-      imgMd: pro_pri_1_md,
-      imgSm: pro_pri_1_sm,
-      imgXs: pro_pri_1_xs,
+      date: "",
+      techs: ["", "", "", "", ""],
+      shortDescription: "A store of clothes and accessories",
+      description: "this is a large description lorem lorem lorem lorem",
+      imgPrincipal: pri_proj_view_04,
+      imgXl: proj_04_xl,
+      imgLg: proj_04_lg,
+      imgMd: proj_04_md,
+      imgSm: proj_04_sm,
+      imgXs: proj_04_xs,
       toURL: "https://fashion-fusion-65eb8.web.app/"
     },
   ];
