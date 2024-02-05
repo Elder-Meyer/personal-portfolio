3f3003965147f8fdd2ba7ee5ac77344efd03bc5f
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-04 20:17:24 -0600

    2.3.307 Update DataContent ProjectDetails Route

--- src/views/projects/ProjectDetails.jsx
+++ src/views/projects/ProjectDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import {Typography, Container, Box, Grid} from '@mui/material'
+import {Typography, Container, Box, Grid, Toolbar, Button } from '@mui/material'
 import { projects } from '../home/Projects/webProjects/projectsData';
 
 export const ProjectDetails = () => {
@@ -16,35 +16,68 @@ export const ProjectDetails = () => {
 
   return (
     <Box sx={{backgroundColor: "background.default"}}>
+
       <Container maxWidth="lg">
-        <Grid container spacing={1}>
-          <Grid item xs={6}>
-            <Typography variant="subtitle1" color="text.primary">
-              {project.id}
-            </Typography>
-          </Grid>
-          <Grid item xs={6}>
-            <Typography variant="subtitle1" color="text.primary">
+        <Grid container columnSpacing={1} rowSpacing={1}>
+          <Grid item xs={12}>
+            <Typography color="primary.light" variant="h4" fontWeight={800} sx={{lineHeight:4}}>
               {project.title}
             </Typography>
           </Grid>
-          <Grid item xs={6}>
-            <Typography variant="subtitle1" color="text.primary">
+          <Grid item xs={12}>
+            <Typography color="text.primary" variant="body1">
               {project.shortDescription}
             </Typography>
           </Grid>
-          <Grid item xs={6}>
-            <Typography variant="subtitle1" color="text.primary">
+          <Grid item xs={12}>
+            <img src={project.imgPrincipal} height="100px" width="700px"/>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography color="primary.light" variant="h5">
+              About
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography color="text.primary" variant="body1">
               {project.description}
             </Typography>
           </Grid>
-          <Grid item xs={6}>
-            <Typography variant="subtitle1" color="text.primary">
+          <Grid item xs={12}>
+            <Typography color="primary.light" variant="h5">
+              Techonologies
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography color="text.primary" variant="body1">elder</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography color="primary.light" variant="h5">
+              Website
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography color="text.primary" variant="body1">
               {project.toURL}
             </Typography>
           </Grid>
+          <Grid item xs={12}>
+            <Typography color="primary.light" variant="h5">
+              Github
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography color="text.primary" variant="body1">
+              enlace a github
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained" color="primary" fullWidth>
+              ir al proyecto
+            </Button>
+          </Grid>
         </Grid>
       </Container>
+
     </Box>
   )
 }
