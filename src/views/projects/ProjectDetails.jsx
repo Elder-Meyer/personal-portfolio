a7a02e924eca5f14a65a1d1ddf12e75fa1ee4aca
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-07 23:46:19 -0600

    2.5.301 Update Data Grid Content Project

--- src/views/projects/ProjectDetails.jsx
+++ src/views/projects/ProjectDetails.jsx
@@ -20,118 +20,102 @@ export const ProjectDetails = () => {
       <Container maxWidth="xl">
         <Paper elevation={0} sx={{p: {xs:1, md:2, lg:3}}}>
           <Grid container spacing={2}>
-
-              {/* TITULO */}
-              <Grid item xs={12}>
-                <Stack direction={{xs:"column", sm: "row"}} spacing={0} display="flex" alignItems="flex-start">
-                  <IconButton aria-label="back"
-                    component={LinkRoute}
-                    to="/projects"
-                  >
-                    <ArrowBackSharp />
-                  </IconButton>
-                  <Typography color="primary.light" variant="h4" fontWeight={800}>
-                    {project.title}
-                  </Typography>
-                </Stack>
-                <Divider />
-              </Grid>
-
-              {/* SUBTITULO */}
-              <Grid item xs={12}>
-                <Typography color="text.primary" variant="body1">
-                  {project.shortDescription}
+            {/* TITULO */}
+            <Grid item xs={12}>
+              <Stack direction={{xs:"column", sm: "row"}} spacing={0} display="flex" alignItems="flex-start">
+                <IconButton aria-label="back" component={LinkRoute} to="/projects">
+                  <ArrowBackSharp />
+                </IconButton>
+                <Typography color="primary.light" variant="h4" fontWeight={800}>
+                  {project.title}
                 </Typography>
-              </Grid>
+              </Stack>
+              <Divider />
+            </Grid>
 
-              {/* USO DE GRID PURO */}
-              {/* GRID - PRINCIPAL DATA */}
-              <Grid container item display="grid" gap={2}
-                gridTemplateRows="auto" //repeat(2, 1fr)
-                gridTemplateColumns="repeat(4, 1fr)"
+            {/* SUBTITULO */}
+            <Grid item xs={12}>
+              <Typography color="text.primary" variant="body1">
+                {project.shortDescription}
+              </Typography>
+            </Grid>
+
+            {/* GRID - PRINCIPAL DATA */}
+            <Grid container item spacing={2}>
+              {/* IMAGE CONTAINER */}
+              <Grid item xs={12} sm={12} md={6} lg={7} xl={8}
+                component={Stack}
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                spacing={1}
               >
-                {/* IMAGEN */}
-                <Grid container item
-                  gridRow={{ xs: "1", md: "1 / span 2", lg: "span 2" }}
-                  gridColumn={{ xs: "1 / -1", md: "1 / span 2", lg: "span 3" }}
-                >
-                  <Card elevation={3} sx={{ maxWidth: "100%" }}>
-                    <CardMedia
-                      component="img"
-                      height="180"
-                      image={project.imgPrincipal}
-                      alt={project.id}
-                    />
-                  </Card>
+                <Card elevation={3}>
+                  <CardMedia component="img"
+                    height="180"
+                    image={project.imgPrincipal}
+                    alt={project.id}
+                  />
+                </Card>
+              </Grid>
+              {/* PRINCIPAL DATA CONTAINER */}
+              <Grid container item xs={12} sm={12} md={6} lg={5} xl={4}>
+                {/* ABOUT - INFO */}
+                <Grid item xs={12}>
+                  <Stack spacing={1}>
+                    <Typography color="text.secondary" variant="caption" display="flex" justifyContent="flex-end">
+                      {project.date}
+                    </Typography>
+                    <Divider />
+                    <Typography color="primary.light" variant="h5" fontWeight={400}>
+                      About
+                    </Typography>
+                    <Typography color="text.primary" variant="body1" sx={{textWrap: "pretty"}}>
+                      {project.largeDescription}
+                    </Typography>
+                  </Stack>
                 </Grid>
-
-                {/* SOBRE */}
-                <Grid container item
-                  gridRow={{ xs: "2", md: "1", lg: "1" }}
-                  gridColumn={{ xs: "1 / -1", md: "3 / span 2", lg: "4" }}
-                >
-                  <Grid item xs={12}>
-                    <Stack spacing={1}>
-                      <Typography color="text.secondary" variant="caption" display="flex" justifyContent="flex-end">
-                        {project.date}
-                      </Typography>
-                      <Divider />
-                      <Typography color="primary.light" variant="h5" fontWeight={400}>
-                        About
-                      </Typography>
-                      <Typography color="text.primary" variant="body1">
-                        {project.largeDescription}
-                      </Typography>
+                {/* TECHNOLOGIES DATA CONTAINER */}
+                <Grid item xs={12} component={Stack}>
+                  <Stack spacing={1} justifyContent={"flex-end"}>
+                    <Typography color="primary.light" variant="h5">
+                      Technologies
+                    </Typography>
+                    <Stack direction="row" flexWrap="wrap" gap={0.5}>
+                      { project && project.techs 
+                        ? project.techs.map((tech, index) => (
+                          <Chip key={index}
+                            label={tech}
+                            variant="filled" 
+                          />
+                        ))
+                        : "No existe la propiedad techs"
+                      }
                     </Stack>
-                  </Grid>
-                </Grid>
-
-                {/* TECNOLOGÍAS */}
-                <Grid container item
-                  gridRow={{ xs: "3", md: "2", lg: "2" }}
-                  gridColumn={{ xs: "1 / -1", md: "3 / span 2", lg: "4" }}
-                >
-                  <Grid item xs={12}>
-                    <Stack spacing={1}>
-                      <Typography color="primary.light" variant="h5">
-                        Technologies
-                      </Typography>
-                      <Stack direction="row" flexWrap="wrap" gap={0.5}>
-                        { project && project.techs 
-                            ? project.techs.map((tech, index) => (
-                              <Chip 
-                                key={index}
-                                label={tech}
-                                variant="filled" 
-                              />
-                            ))
-                            : "No existe la propiedad techs"
-                        }
-                      </Stack>
-                    </Stack>
-                  </Grid>
+                  </Stack>
                 </Grid>
               </Grid>
+            </Grid>
 
-              {/* WEBSITE */}
-              <Grid item xs={12} sm={6}>
-                <Stack direction="row" spacing={1} alignItems="center">
-                  <Language color="primary"/>
-                  <Link color="primary" variant="h5" href={project.href} target="_BLANK" underline="always">
-                    Website
-                  </Link>
-                </Stack>
-              </Grid>
+            {/* WEBSITE */}
+            <Grid item xs={12} sm={6}>
+              <Stack direction="row" spacing={1} alignItems="center">
+                <Language color="primary"/>
+                <Link color="primary" variant="h5" href={project.href} target="_BLANK" underline="always">
+                  Website
+                </Link>
+              </Stack>
+            </Grid>
 
-              {/* GITHUB */}
-              <Grid item xs={12} sm={6}>
-                <Stack direction="row" spacing={1} alignItems="center">
-                  <GitHub color="primary"/>
-                  <Link color="primary" variant="h5" href={project.repo} target="_BLANK" underline="always">
-                    GitHub
-                  </Link>
-                </Stack>
-              </Grid>
+            {/* GITHUB */}
+            <Grid item xs={12} sm={6}>
+              <Stack direction="row" spacing={1} alignItems="center">
+                <GitHub color="primary"/>
+                <Link color="primary" variant="h5" href={project.repo} target="_BLANK" underline="always">
+                  GitHub
+                </Link>
+              </Stack>
+            </Grid>
           </Grid>
         </Paper>          
       </Container>
