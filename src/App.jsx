28b09d209a383c1345c3f2321e4d16388c6a25e4
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-08 21:09:44 -0600

    2.6.2 Remove Unnecesary Code

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import ReactGA from 'react-ga';
 const TRACKING_ID = "6395311067"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID, {debug: true});
 
-console.log("welcome to my portfolio")
-
 function App() {
   return (
     <ThemeProvider theme={lightTheme}>
