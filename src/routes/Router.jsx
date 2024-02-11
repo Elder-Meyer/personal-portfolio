44a97533accb8393a66be2c20fd784bea281fafe
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-11 15:48:45 -0600

    2.6.6 Update Router Perfor

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,15 +1,16 @@
 import React, { Suspense, lazy } from 'react'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import '../styles/ScrollbarStyle.css'
-const Home            = lazy(() => import('../views/home/Home').then((module) => ({ default: module.Home })))
+import { Home } from '../views/home/Home'
+// const Home            = lazy(() => import('../views/home/Home').then((module) => ({ default: module.Home })))
 const Projects        = lazy(() => import('../views/projects/Projects').then((module) => ({ default: module.Projects })))
 const ProjectDetails  = lazy(() => import('../views/projects/ProjectDetails').then((module) => ({ default: module.ProjectDetails })))
 const Blog            = lazy(() => import('../views/blog/Blog').then((module) => ({ default: module.Blog })))
 const Contact         = lazy(() => import('../views/contact/Contact').then((module) => ({ default: module.Contact })))
 const Error           = lazy(() => import('../views/error/Error').then((module) => ({ default: module.Error })))
-const DrawerAppBar    = lazy(() => import('../components/layout/navbar/DrawerAppBar').then((module) => ({ default: module.DrawerAppBar })))
-const WavyDivider     = lazy(() => import('../components/items/WavyDivider').then((module) => ({ default: module.WavyDivider })))
-const AppFooter       = lazy(() => import('../components/layout/footer/AppFooter').then((module) => ({ default: module.AppFooter })))
+import { DrawerAppBar } from '../components/layout/navbar/DrawerAppBar'
+import { WavyDivider } from '../components/items/WavyDivider'
+import { AppFooter } from '../components/layout/footer/AppFooter'
 import { Fab } from '@mui/material'
 import { BtnScrollTop } from '../components/items/btnScrollTop'
 import { KeyboardArrowUp } from '@mui/icons-material'
@@ -20,67 +21,29 @@ import { Box } from '../components/material-ui/Box'
 
 export const Router = (props) => {
   return (
-    <Box sx={{backgroundColor: "background.paper"}}>
-      <Suspense fallback={<Loader/>}>
-        <BrowserRouter>
-          <DrawerAppBar />
-          <Box id="back-to-top-anchor" />
-
-          <ScrollToTop>
+    <BrowserRouter>
+      <Box id="back-to-top-anchor" />
+      <DrawerAppBar />
+        <ScrollToTop>
+          <Suspense fallback={<Loader/>}>
             <Routes>
-              <Route 
-                path='/'                         
-                element={<Navigate to='/home'/>}
-              />
-              <Route 
-                path='/home/'                    
-                element={
-                    <Home/>
-                } 
-              />
-              <Route 
-                path='/projects'
-                element={
-                  <Projects/>
-                }
-              />
-              <Route 
-                path="/projects/:id"
-                element={
-                  <ProjectDetails/>
-                }
-              />
-              <Route 
-                path='/blog/'                    
-                element={
-                    <Blog/>
-                } 
-              />
-              <Route 
-                path='/contact/'                 
-                element={
-                    <Contact/>
-                } 
-              />
-              <Route 
-                path='*'                         
-                element={
-                    <Error/>
-                } 
-              />
+              <Route path='/'             element={<Navigate to='/home'/>} />
+              <Route path='/home/'        element={<Home/>} />
+              <Route path='/projects'     element={<Projects/>} />
+              <Route path="/projects/:id" element={<ProjectDetails/>} />
+              <Route path='/blog/'        element={<Blog/>} />
+              <Route path='/contact/'     element={<Contact/>} />
+              <Route path='*'             element={<Error/>} />
             </Routes>
-
-            <BtnScrollTop {...props}>
-              <Fab size="small" aria-label="scroll back to top" color='primary'>
-                <KeyboardArrowUp/>
-              </Fab>
-            </BtnScrollTop>
-          </ScrollToTop>
-            
-          <WavyDivider/>
-          <AppFooter/>
-        </BrowserRouter>
-      </Suspense>
-    </Box>
+          </Suspense>
+          <BtnScrollTop {...props}>
+            <Fab size="small" aria-label="scroll back to top" color='primary'>
+              <KeyboardArrowUp/>
+            </Fab>
+          </BtnScrollTop>
+        </ScrollToTop>
+        <WavyDivider/>
+        <AppFooter/>
+    </BrowserRouter>
   )
 }
