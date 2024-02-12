ace743ddbacbc3e1ba419a4a234a006fe714f614
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-12 11:04:46 -0600

    2.7.1 Update Theme Color Palette light / dark

--- src/styles/ThemeMui.jsx
+++ src/styles/ThemeMui.jsx
@@ -1,36 +1,39 @@
-import {createTheme} from "@mui/material";
+import { createTheme } from "@mui/material";
 
 export const lightTheme = createTheme({
-    palette: {
-      mode: 'dark',
-      primary:{
-        main: "#bcb5ff",
-        light: "#cac3ff",
-        dark: "#aea7ff"
-      },
-      secondary:{
-        main: "#8C6DB5",
-        light: "#f0dbff",
-        dark: "#ffffff"
-      },
-      text:{
-        primary: "#FFFFFF",
-        secondary: "#e0e0e0",
-        icon: "#fb7703",
-      },
-      divider: "#59517a",
-      background:{
-        paper: "#32313a",
-        default: "#1d1c26"
-      }
+  palette: {
+    mode: "light"
+  },
+  typography: {
+    fontFamily: "'Plus Jakarta Sans', sans-serif",
+  },
+});
 
-    },
-    typography:{
-      fontFamily: "'Plus Jakarta Sans', sans-serif"
-    }
-  });
-  
 export const darkTheme = createTheme({
-    
-  });
-
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#bcb5ff",
+      light: "#cac3ff",
+      dark: "#aea7ff",
+    },
+    secondary: {
+      main: "#8C6DB5",
+      light: "#f0dbff",
+      dark: "#ffffff",
+    },
+    text: {
+      primary: "#FFFFFF",
+      secondary: "#e0e0e0",
+      icon: "#fb7703",
+    },
+    divider: "#59517a",
+    background: {
+      paper: "#32313a",
+      default: "#1d1c26",
+    },
+  },
+  typography: {
+    fontFamily: "'Plus Jakarta Sans', sans-serif",
+  },
+});
