b01d2d1e0a12161a34b7fb078d71e89430b1fa8b
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-13 09:45:26 -0600

    2.701.0 Update provisional thememui light

--- src/styles/ThemeMui.jsx
+++ src/styles/ThemeMui.jsx
@@ -2,7 +2,27 @@ import { createTheme } from "@mui/material";
 
 export const lightTheme = createTheme({
   palette: {
-    mode: "light"
+    mode: "light",
+    primary: {
+      main: "#4e88ca",
+      light: "#0eabee",
+      dark: "#005B99",
+    },
+    secondary: {
+      main: "#8C6DB5",
+      light: "#f0dbff",
+      dark: "#ffffff",
+    },
+    text: {
+      primary: "#333333",
+      secondary: "#5c5c5c",
+      icon: "#fb7703",
+    },
+    divider: "#c2c2c2",
+    background: {
+      paper: "#ebebeb",
+      default: "#F5F5F5",
+    },
   },
   typography: {
     fontFamily: "'Plus Jakarta Sans', sans-serif",
