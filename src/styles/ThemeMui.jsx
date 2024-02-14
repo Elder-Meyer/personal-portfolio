59cccd111d5d29c65ae56caaad7627665eea9258
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-14 01:15:42 -0600

    2.7.105 Update light Theme MUI

--- src/styles/ThemeMui.jsx
+++ src/styles/ThemeMui.jsx
@@ -4,24 +4,24 @@ export const lightTheme = createTheme({
   palette: {
     mode: "light",
     primary: {
-      main: "#4e88ca",
-      light: "#0eabee",
-      dark: "#005B99",
+      main: "#5c62e0",
+      light: "#7d7dff",
+      dark: "#565eda",
     },
     secondary: {
-      main: "#8C6DB5",
-      light: "#f0dbff",
-      dark: "#ffffff",
+      main: "#6794ff",
+      light: "#565eda",
+      dark: "#003d9a",
     },
     text: {
       primary: "#333333",
       secondary: "#5c5c5c",
       icon: "#fb7703",
     },
-    divider: "#c2c2c2",
+    divider: "#cccccc",
     background: {
-      paper: "#ebebeb",
-      default: "#F5F5F5",
+      paper: "#F5F5F5",
+      default: "#FFFFFF",
     },
   },
   typography: {
