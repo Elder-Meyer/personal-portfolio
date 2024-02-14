7e74ceea66cbad67ab06fd285e97c0fd6174a666
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-14 01:06:52 -0600

    2.7.104 Add CssBaseline change scroll style

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import ReactGA from 'react-ga';
 import { useState } from "react";
 const TRACKING_ID = "6395311067"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID, {debug: true});
+import { CssBaseline } from '@mui/material';
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
   const handleChangeTheme = () => { setDarkMode(!darkMode); };
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <CssBaseline enableColorScheme/>
       <meta name="theme-color" content={darkMode ? darkTheme.palette.background.default : lightTheme.palette.background.default } />
       <Router darkMode={darkMode} handleChangeTheme={handleChangeTheme}/>
     </ThemeProvider>
