194fc389902e24da9f26149fb8cc3aa1d61c4a74
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-15 11:55:51 -0600

    3.0.104 Add language useTranslation-- Error

--- src/views/error/Error.jsx
+++ src/views/error/Error.jsx
@@ -7,8 +7,10 @@ import { Box } from '../../components/material-ui/Box'
 import { Button } from '../../components/material-ui/Button'
 import { Container } from '../../components/material-ui/Container'
 import { Grid } from '../../components/material-ui/Grid'
+import { useTranslation } from 'react-i18next'
 
 export const Error = () => {
+    const { t } = useTranslation();
   return (
     <Box sx={{backgroundColor: "background.default"}}>
         <Container maxWidth="lg">
@@ -27,12 +29,12 @@ export const Error = () => {
                         gutterBottom
                         sx={{fontSize: {xs: 40, sm: 50, md:60}, fontWeight: 700, color: "primary.main"}}
                     >
-                        There is nothing here. 
+                        {t('error.title')}
                     </Typography>
                 </Grid>
                 <Grid item xs={12} display={"flex"} justifyContent={"center"}>
                     <Button variant='contained' component={Link} to="/" startIcon={<ArrowBack/>}>
-                        back to home
+                        {t('error.btn')}
                     </Button>
                 </Grid>
             </Grid>
