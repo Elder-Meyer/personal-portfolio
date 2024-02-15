78671407c52170c739895e1aa9cb6140b53db27a
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-15 02:14:49 -0600

    2.8.106 Add main import i18n

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { CssBaseline } from '@mui/material';
+import './config/lan/i18n.jsx'
 
 const container = document.getElementById('root');
 const root = createRoot(container);
