01fe65aa976579218e9e12fb8f1504143fe051cc
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-17 04:22:43 -0600

    3.1.114 Update Home Structure Sections

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,6 +1,6 @@
 import React, { lazy } from 'react'
 const Presentation = lazy(() => import('./Presentation/Presentation').then((module) => ({ default:module.Presentation })))
-const SkillAndLanguages = lazy(() => import('./SkillAndLanguages/SkillAndLanguages').then((module) => ({ default:module.SkillAndLanguages })))
+const Skills = lazy(() => import('./Skills/Skills').then((module) => ({ default:module.Skills })))
 const Projects = lazy(() => import('../projects/Projects').then((module) => ({ default:module.Projects })))
 // MATERIAL UI - LOCAL
 import { Box } from '../../components/material-ui/Box'
@@ -10,7 +10,7 @@ export const Home = () => {
     <Box>
       <Presentation/>  
       <Projects/>
-      <SkillAndLanguages/>    
+      <Skills/>    
     </Box>
   )
 }
