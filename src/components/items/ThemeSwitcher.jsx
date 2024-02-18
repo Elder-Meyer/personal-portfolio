75cab1cc429283af4cb404efb48a418397128d64
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-17 22:14:46 -0600

    3.1.212 Add t Translations ThemeSwitcherr

--- src/components/items/ThemeSwitcher.jsx
+++ src/components/items/ThemeSwitcher.jsx
@@ -1,33 +1,40 @@
 import React, { useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import { DarkMode, DarkModeOutlined, Face, LightMode, LightModeOutlined } from '@mui/icons-material';
+import { Tooltip } from '../material-ui/Tooltip';
+import { useTranslation } from 'react-i18next';
 
 const ThemeSwitcher = (props) => {
+
+  const { t } = useTranslation()
     
-    const { darkMode } = props
-    const { handleChangeTheme } = props
+  const { darkMode } = props
+  const { handleChangeTheme } = props
 
   const [hovered, setHovered] = useState(false);
 
   return (
-    <IconButton
-      aria-label=""
-      onClick={handleChangeTheme}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-      sx={{
-        borderWidth: 1,
-        borderStyle: "solid",
-        borderColor: "inherit",
-        color : "inherit"
-      }}
-    >
-      {darkMode ? (
-        hovered ? <DarkMode /> : <DarkModeOutlined />
-      ) : (
-        hovered ? <LightMode /> : <LightModeOutlined />
-      )}
-    </IconButton>
+    <Tooltip title={t("navbar.themebtn")} placement='bottom' arrow>
+      <IconButton
+        aria-label=""
+        onClick={handleChangeTheme}
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        sx={{
+          borderWidth: 1,
+          borderStyle: "solid",
+          borderColor: "inherit",
+          color : "inherit"
+        }}
+      >
+
+          {darkMode ? (
+            hovered ? <DarkMode /> : <DarkModeOutlined />
+          ) : (
+            hovered ? <LightMode /> : <LightModeOutlined />
+          )}
+      </IconButton>
+    </Tooltip>
   );
 };
 
