bf906cfeb9c1eb6971f576a4faf1c3e4a003e323
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-20 22:28:57 -0600

    3.2.103 Add Divider between Presentation Projects Skills

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -4,12 +4,15 @@ const Skills = lazy(() => import('./Skills/Skills').then((module) => ({ default:
 const Projects = lazy(() => import('../projects/Projects').then((module) => ({ default:module.Projects })))
 // MATERIAL UI - LOCAL
 import { Box } from '../../components/material-ui/Box'
+import { Divider } from '../../components/material-ui/Divider'
 
 export const Home = () => {
   return (
     <Box>
       <Presentation/>  
+      <Divider variant="middle" />
       <Projects/>
+      <Divider variant="middle" />
       <Skills/>    
     </Box>
   )
