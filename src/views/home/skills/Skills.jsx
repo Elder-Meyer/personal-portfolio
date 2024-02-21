2293d3ec5a98032d3f1e1e91a6195627ed6d6565
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-21 00:15:59 -0600

    3.2.105 Update Structure Skills Component

--- src/views/home/skills/Skills.jsx
+++ src/views/home/skills/Skills.jsx
@@ -11,26 +11,20 @@ import { Stack } from '../../../components/material-ui/Stack'
 export const Skills = () => {
   const { t } = useTranslation();
   return (
-    <Box component='section' sx={{ bgcolor: "background.default",  px:{ xs: 0, sm:1, md:2, lg:2}, py: {xs:5, sm:5, md:6} }}>
+    <Box component='section' sx={{ bgcolor: "background.default",  px:{ xs: 0, sm:1, md:2, lg:2}, py: {xs:3, md:10, xl: 20} }}>
       <Container maxWidth="lg">
-        <Paper sx={{borderRadius: 4, py: 3, px: {xs: 2, sm:2, md:2, lg: 8, xl:16 }, bgcolor: "background.paper" }} elevation={0}>
-          <Typography color='text.primary' textAlign="center" sx={{ fontSize: 40, fontWeight: 500}} gutterBottom>
-            {t('home.skills.titleskills')}
-          </Typography>
-          <Typography color='text.secondary' textAlign="center" sx={{fontSize: 18, my:3}}>
-            {t('home.skills.descriptionskills')}
-          </Typography>
-
+        <Paper sx={{borderRadius: 6, py: 3, px: {xs: 1.5, md: 8, xl:16 }, bgcolor: "background.paper" }} elevation={0}>
+          <Stack direction="column" spacing={1} pb={2}>
+            <Typography color='primary.main' textAlign="center" sx={{ fontSize: 40, fontWeight: 500}}>
+              {t('home.skills.titleskills')}
+            </Typography>
+            <Typography color='text.secondary' textAlign="center" sx={{fontSize: 18}}>
+              {t('home.skills.descriptionskills')}
+            </Typography>
+          </Stack>
           <Grid container>
             <Grid item xs={12}>
-              <Stack
-                direction="row"
-                justifyContent="center"
-                alignItems="flex-start"
-                spacing={0.5}
-                useFlexGap
-                flexWrap="wrap"
-              >
+              <Stack direction="row" justifyContent="center" alignItems="flex-start" spacing={0.5} useFlexGap flexWrap="wrap">
                 <span>
                   <span className="bg-violet-700/10 inline-flex text-primary py-0.5 px-3 items-center justify-between text-sm capitalize border border-violet-400 rounded-full hover:bg-gray-gray1 mb-2 mr-2">
                     <svg alt=" " className="object-cover w-6 h-6 mr-1" style={{color: "transparent"}} viewBox="0 0 256 204" xmlns="http://www.w3.org/2000/svg" width="16" height="16" preserveAspectRatio="xMidYMid"><path fill="#7E13F8" d="M53.172 0C38.565 0 27.756 12.785 28.24 26.65c.465 13.32-.139 30.573-4.482 44.642C19.402 85.402 12.034 94.34 0 95.488v12.956c12.034 1.148 19.402 10.086 23.758 24.197 4.343 14.069 4.947 31.32 4.482 44.641-.484 13.863 10.325 26.65 24.934 26.65h149.673c14.608 0 25.414-12.785 24.93-26.65-.464-13.32.139-30.572 4.482-44.641 4.359-14.11 11.707-23.05 23.741-24.197V95.488c-12.034-1.148-19.382-10.086-23.74-24.196-4.344-14.067-4.947-31.321-4.483-44.642C228.261 12.787 217.455 0 202.847 0H53.17h.002ZM173.56 125.533c0 19.092-14.24 30.67-37.872 30.67h-40.23a4.339 4.339 0 0 1-4.338-4.339V52.068a4.339 4.339 0 0 1 4.339-4.34h39.999c19.705 0 32.637 10.675 32.637 27.063 0 11.503-8.7 21.801-19.783 23.604v.601c15.089 1.655 25.248 12.104 25.248 26.537Zm-42.26-64.05h-22.937v32.4h19.32c14.934 0 23.17-6.014 23.17-16.764 0-10.073-7.082-15.636-19.552-15.636Zm-22.937 45.256v35.705h23.782c15.548 0 23.786-6.239 23.786-17.965 0-11.728-8.467-17.742-24.786-17.742h-22.782v.002Z"/></svg>
