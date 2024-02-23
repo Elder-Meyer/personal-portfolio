7f597675e87dc5d7cf5dc7a390168c5d01b2d3da
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-23 13:26:47 -0600

    3.301.0 Update Footer Items Structure

--- src/components/layout/footer/AppFooter.jsx
+++ src/components/layout/footer/AppFooter.jsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
 import { Link as LinkRoute } from 'react-router-dom';
-import { TextField, useTheme } from '@mui/material';
+import { TextField, useTheme, MenuItem } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import cube from '../../../assets/img/principal/cube-glow.webp';
 import UseAnimations from 'react-useanimations';
 import facebook from "react-useanimations/lib/facebook";
 import twitter from "react-useanimations/lib/twitter";
 import github from "react-useanimations/lib/github";
+import linkedin from "react-useanimations/lib/linkedin"
 import useAnalyticsEventTracker from '../../../config/analytics/useAnalyticsEventTracker';
 import { Typography } from '../../material-ui/Typography';
 import { Box } from '../../material-ui/Box';
@@ -17,15 +19,12 @@ import { Grid } from '../../material-ui/Grid';
 import { Link } from '../../material-ui/Link';
 import { useTranslation } from 'react-i18next';
 import { Stack } from '../../material-ui/Stack';
+import { Divider } from '../../material-ui/Divider';
 
-function Copyright({theme}) {
-  return (
-    <Box sx={{display: "flex", justifyContent: "center", color: theme.palette.mode==='dark'?"text.secondary":"background.paper"}}>
-      <Link color="inherit" component={LinkRoute} to="/home">
-        {'© Elder Meyer '}{ new Date().getFullYear() }
-      </Link>
-    </Box>
-  );
+function Copyright({theme, t}) {
+  return ( <Box sx={{color: theme.palette.mode==='dark'?"text.secondary":"background.paper"}}>
+      <Typography variant="caption"> {'©  '}{ new Date().getFullYear() }{" Elder Meyer, "}{t("footer.copy")} </Typography>
+  </Box> );
 }
 
 const Alert = React.forwardRef(function Alert(props, ref) {
@@ -37,6 +36,7 @@ export const AppFooter = () => {
   const { i18n, t } = useTranslation();
 
   const [openAlert, setOpenAlert] = React.useState(false);
+  const [selectedLanguage, setSelectedLanguage] = React.useState(i18n.language);
 
   const handleClick = () => { setOpenAlert(true); };
 
@@ -48,7 +48,8 @@ export const AppFooter = () => {
   const onChangeLang = (e) => {
     const lang_code = e.target.value;
     i18n.changeLanguage(lang_code);
-    handleClick()
+    setSelectedLanguage(lang_code);
+    handleClick();
   };
 
   const LANGUAGES = [
@@ -66,96 +67,68 @@ export const AppFooter = () => {
       </Snackbar>
       
       <Container sx={{ my: 8 }}>
-        <Grid container spacing={3}>
-          
-          <Grid item xs={12} sm={4} md={3} order={{md: 1, sm:1, xs:4}}>
-            <Stack direction="column" spacing={0.5}>
-              <Stack direction="row" justifyContent="center">
-                <LinkRoute to="https://facebook.com/yahir.meyer" target='_BLANK'>
-                  <UseAnimations 
-                    animation={facebook} 
-                    size={56} 
-                    strokeColor={theme.palette.mode === "dark" ?  theme.palette.primary.light : theme.palette.background.default} 
-                  />
-                </LinkRoute>
-                <LinkRoute to="https://twitter.com/elder_yahir" target='_BLANK'>
-                  <UseAnimations 
-                    animation={twitter} 
-                    size={56} 
-                    strokeColor={theme.palette.mode === "dark" ?  theme.palette.primary.light : theme.palette.background.default} 
-                  />
-                </LinkRoute>
-                <LinkRoute to="https://github.com/Elder-Meyer" target='_BLANK'>
-                  <UseAnimations 
-                    animation={github} 
-                    size={56} 
-                    strokeColor={theme.palette.mode === "dark" ?  theme.palette.primary.light : theme.palette.background.default} 
-                  />
-                </LinkRoute>
-              </Stack>
-              <Copyright theme={theme} />
+        <Grid container spacing={1}>
+          <Grid item xs={12} md={4}>
+            <Stack direction="row" spacing={1} height={"100%"} justifyContent={{xs: "center", md: "flex-start"}} alignItems="center" mx={2}>
+              <CardMedia component="img"
+                sx={{ width: 42, bgcolor: "transparent" }}
+                image={cube} alt="just a simple cube from GD" title='just a cube'
+              />
+              <Typography variant='h5' fontWeight={700} color={theme.palette.mode === "dark" ? "primary.main" : "background.paper"}> Elder </Typography>
             </Stack>
           </Grid>
-          
-          <Grid item xs={12} sm={4} md={2} order={{md: 2, sm:2, xs:1}}>
-            <Stack direction="column" spacing={0.5}>
-              <Typography color={theme.palette.mode==='dark'?"primary.light":"background.default"} variant="h6" marked="left" gutterBottom>
-                {t('footer.legal.title')}
-              </Typography>
-              <Link color="inherit" variant='subtitle1' component={LinkRoute} to="/terms">{t('footer.legal.terms')}</Link>
-              <Link color="inherit" variant='subtitle1' component={LinkRoute} onClick={()=> gaEventTracker('privacy')} to="/privacy">{t('footer.legal.privacy')}</Link>
-              <Link color="inherit" variant='subtitle1' component={LinkRoute} onClick={()=> gaEventTracker('call')} to="/contact">{t('footer.legal.about')}</Link>
-            </Stack>
-          </Grid>
-          
-          <Grid item xs={12} sm={8} md={4} order={{md: 3, sm:3, xs:2}}>
-            <Stack direction="column" spacing={0.5}>
-              <Typography color={theme.palette.mode==='dark'?"primary.light":"background.default"} variant="h6" marked="left" gutterBottom>
-                {t("footer.language.title")}
-              </Typography>
-              <TextField
+          <Grid item xs={12} md={8}>
+            <Stack direction="row" spacing={3} height={"100%"} flexWrap={"wrap"} justifyContent={{xs: "center", md: "flex-end"}} alignItems={"center"} mx={2} color={theme.palette.mode === "dark" ? "primary.light" : "background.default"}>
+              <Link color="inherit" variant='subtitle1' underline='hover' component={LinkRoute} to='/about-me' onClick={() => gaEventTracker('About me')}> {t('footer.links.about')} </Link>
+              <Link color="inherit" variant='subtitle1' underline='hover' component={LinkRoute} to='/blog'     onClick={() => gaEventTracker('blog')}    > {t('footer.links.blog')} </Link>
+              <Link color="inherit" variant='subtitle1' underline='hover' component={LinkRoute} to='/contact'  onClick={() => gaEventTracker('contact')} > {t('footer.links.contact')} </Link>
+              
+              <CssTextField 
                 onChange={onChangeLang}
                 select
-                size="medium"
-                variant="standard"
-                defaultValue={i18n.language}
+                label={t("footer.language.title")}
+                size="small"
+                value={selectedLanguage}
                 SelectProps={{
-                  native: true,
-                }}
-                sx={{ 
-                  width: 150,
-                  '& .MuiInputBase-root': {
-                    color: theme.palette.mode==='dark'?"text.secondary":"primary.dark", 
-                    bgcolor: theme.palette.mode==='dark'?"background.paper":"background.paper", 
-                    
-                  },
-                  '& .MuiSelect-root': {
-                    bgcolor: theme.palette.mode==='dark'?"text.secondary":"green", 
-                  },
+                  MenuProps: {disableScrollLock: true},
                 }}
               >
-                {LANGUAGES.map(({ code, label }) => (
-                  <option key={code} value={code}>
-                    {label}
-                  </option>
+                {LANGUAGES.map((option) => (
+                  <MenuItem key={option.code} value={option.code}>
+                    {option.label}
+                  </MenuItem>
                 ))}
-              </TextField>
+              </CssTextField>
             </Stack>
           </Grid>
-          
-          <Grid item xs={12} sm={4} md={3} order={{md: 4, sm:4, xs:3}}>
-            <Stack direction="row" justifyContent={"center"}>
-              <CardMedia
-                sx={{
-                  width: 56,
-                  bgcolor: "transparent",
-                }}
-                component="img"
-                height={"100%"}
-                image={cube}
-                alt="just a simple cube from GD"
-                title='just a cube'
-              />
+          <Grid item xs={12}> <Divider color="inherit"/> </Grid>
+          <Grid item xs={12} sm={6}>
+            <Stack direction="row" spacing={1} height={"100%"} justifyContent={{xs:"center", sm: "flex-start"}} alignItems="center" mx={2}>
+              <Copyright theme={theme} t={t} />
+            </Stack>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Stack direction="row" spacing={0.5} height={"100%"} justifyContent={{xs: "center", sm: "flex-end"}} alignItems={"center"} mx={2} color={theme.palette.mode === "dark" ? "primary.light" : "background.paper"}>
+              <LinkRoute to="https://www.linkedin.com/in/elder-meyer-196a3a290/" target='_BLANK'>
+                <UseAnimations animation={linkedin} size={45} 
+                  strokeColor={theme.palette.mode === "dark" ?  theme.palette.primary.light : theme.palette.background.default} 
+                />
+              </LinkRoute>
+              <LinkRoute to="https://twitter.com/elder_yahir" target='_BLANK'>
+                <UseAnimations animation={twitter} size={45} 
+                  strokeColor={theme.palette.mode === "dark" ?  theme.palette.primary.light : theme.palette.background.default} 
+                />
+              </LinkRoute>
+              <LinkRoute to="https://github.com/Elder-Meyer" target='_BLANK'>
+                <UseAnimations animation={github} size={45} 
+                  strokeColor={theme.palette.mode === "dark" ?  theme.palette.primary.light : theme.palette.background.default} 
+                />
+              </LinkRoute>
+              <LinkRoute to="https://facebook.com/yahir.meyer" target='_BLANK'>
+                <UseAnimations animation={facebook} size={45} 
+                  strokeColor={theme.palette.mode === "dark" ?  theme.palette.primary.light : theme.palette.background.default} 
+                />
+              </LinkRoute>
             </Stack>
           </Grid>
         </Grid>
@@ -163,3 +136,30 @@ export const AppFooter = () => {
     </Box>
   );
 }
+
+const CssTextField = styled(TextField)(({ theme })=>({
+  width: 115,
+  '& label ': {
+    color: theme.palette.mode === "dark" ? theme.palette.primary.light : theme.palette.background.default,
+  },
+  '& label.Mui-focused': {
+    color: theme.palette.mode === "dark" ? theme.palette.primary.light : theme.palette.background.default,
+  },
+  '.MuiSelect-select':{
+    color: theme.palette.mode === "dark" ? theme.palette.primary.light : theme.palette.background.default,
+  },
+  '.MuiSvgIcon-root':{
+    color: "white",
+  },
+  '& .MuiOutlinedInput-root': {
+    '&:hover fieldset': {
+      borderColor: theme.palette.mode === "dark" ? theme.palette.primary.main : theme.palette.background.paper,
+    },
+    '& fieldset': {
+      borderColor: theme.palette.mode === "dark" ? theme.palette.primary.light : theme.palette.background.default,
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: theme.palette.mode === "dark" ? theme.palette.primary.light : theme.palette.background.default,
+    },
+  },
+}));
