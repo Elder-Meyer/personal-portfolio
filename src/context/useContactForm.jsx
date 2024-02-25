458169a0c1040ff87ce8d0da1729e2ba363caecd
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-25 04:50:48 -0600

    3.4.305 Update useContactForm logic submit and notworking

--- src/context/useContactForm.jsx
+++ src/context/useContactForm.jsx
@@ -3,33 +3,26 @@ import { useFormik }              from "formik";
 import emailjs                    from '@emailjs/browser'
 
 export const useContactForm = ({ setTextAlert, setSnackbarOpen, setVariantAlert, form, t }) => {
-  const handleSubmit = async (data) => {
-    console.log(data)
-    sendEmail(form, setVariantAlert, setTextAlert, setSnackbarOpen, t)
-  };
-
   const formikBag = useFormik({
     initialValues: {
-      name : "",
-      email : "",
+      name: "",
+      email: "",
       message: ""
     },
     validationSchema: Yup.object({
       name: Yup.string().required(t("contact.form.helpname")),
       email: Yup.string().email(t("contact.form.helpemailtype")).required(t("contact.form.helpemail")),
-    //   message: Yup.string().required("El mensaje es obligatorio"),
     }),
-    onSubmit: (formData) => {
-      formikBag.handleReset();
-      handleSubmit(formData);
+    onSubmit: (formData, { setSubmitting, resetForm }) => {
+      sendEmail(form, setVariantAlert, setTextAlert, setSnackbarOpen, t, formData, setSubmitting, resetForm);
     },
   });
 
   return formikBag;
 };
 
-
-const sendEmail = (form, setVariantAlert, setTextAlert, setSnackbarOpen, t) => {
+const sendEmail = (form, setVariantAlert, setTextAlert, setSnackbarOpen, t, data, setSubmitting, resetForm) => {
+  console.log(data)
   emailjs
     .sendForm('service_sbks8j7', 'template_poglmy6', form.current, {
       publicKey: 'Dp0PXdHBw9twu_g_e',
@@ -40,14 +33,17 @@ const sendEmail = (form, setVariantAlert, setTextAlert, setSnackbarOpen, t) => {
         console.log("message sent");
         setVariantAlert("success")
         setTextAlert(t("contact.alert.success"));
-        setSnackbarOpen(true)
+        setSnackbarOpen(true);
+        setSubmitting(false); 
+        resetForm();
       },
       (error) => {
         console.log(error.text);
         console.error("error");
         setVariantAlert("error")
         setTextAlert(t("contact.alert.error"));
-        setSnackbarOpen(true)
+        setSnackbarOpen(true);
+        setSubmitting(false);
       }
     );
 };
