6c68ed08db10dfe3baab1a3929748a88cff8ebbe
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-24 22:40:59 -0600

    3.4.301 Update Clean Code fnElevationScroll.jsx Structure

--- src/utils/fnElevationScroll.jsx
+++ src/utils/fnElevationScroll.jsx
@@ -1,18 +1,14 @@
-import * as React from 'react';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
+import { cloneElement } from 'react';
 
 export function ElevationScroll(props) {
     const { children, window } = props;
-    // Note that you normally won't need to set the window ref as useScrollTrigger
-    // will default to window.
-    // This is only being set here because the demo is in an iframe.
+    // Note that you normally won't need to set the window ref as useScrollTrigger. will default to window. This is only being set here because the demo is in an iframe.
     const trigger = useScrollTrigger({
       disableHysteresis: true,
       threshold: 0,
       target: window ? window() : undefined,
     });
   
-    return React.cloneElement(children, {
-      elevation: trigger ? 4 : 0,
-    });
+    return cloneElement(children, { elevation: trigger ? 1 : 0 });
   }
