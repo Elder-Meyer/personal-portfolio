a2a9e969b65cb7ee576d1285fd6bb2c966925a06
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-25 12:09:37 -0600

    3.4.403 Update Blog with example firebase DB

--- src/views/blog/Blog.jsx
+++ src/views/blog/Blog.jsx
@@ -3,9 +3,22 @@ import { Container } from '../../components/material-ui/Container'
 import { Grid } from '../../components/material-ui/Grid'
 import { Typography } from '../../components/material-ui/Typography'
 import { WorkingOnThat } from '../../components/items/WorkingOnThat'
+import { useEffect, useState } from 'react'
+import { getItems } from '../../app/api'
 
 export const Blog = () => {
   const { t } = useTranslation();
+  const [projects, setProjects] = useState(null)
+
+  const getProjectsData = async() => {
+    const p = await getItems();
+    setProjects(p)
+  }
+
+  useEffect(() =>{
+    getProjectsData();
+    console.log("projects =>", projects)
+  }, [])
   return (
     <Container maxWidth="lg" sx={{minHeight: {xs: "55vh", sm: "60vh", md: "67vh"}, display: "flex", alignItems: "center" }}>
       <Grid container columnSpacing={2} py={5}>
