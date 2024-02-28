a5d453cfb815056dbf3ec3dbcd0472c8cdd30b8f
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-28 00:53:50 -0600

    3.4.504 Update Cumulative Layout shift (CLS) - Logo

--- src/components/items/CubeGD.jsx
+++ src/components/items/CubeGD.jsx
@@ -4,7 +4,7 @@ import { CardMedia } from '../material-ui/CardMedia';
 export function CubeGD({animation}){
   return(
     <CardMedia component="img"
-      sx={{ width: 42, bgcolor: "transparent", userSelect: "none" }}
+      sx={{ aspectRatio: "42/42", width: 42, bgcolor: "transparent", userSelect: "none" }}
       image={cube}
       alt="just a simple cube from GD"
       title='just a cube'
