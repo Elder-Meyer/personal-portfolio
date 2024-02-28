6fb2cc7cf32ca54f438952a5d4c40c755a1784ec
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-27 22:35:20 -0600

    3.4.503 Update Breadcrumbs Design Items

--- src/views/error/Error.jsx
+++ src/views/error/Error.jsx
@@ -12,6 +12,9 @@ export const Error = () => {
         <Container maxWidth="lg" sx={{minHeight: {xs: "55vh", sm: "60vh", md: "67vh"}, display: "flex", alignItems: "center" }}>
             <Grid container columnSpacing={2} py={10}>
                 <Grid item xs={12}>
+                    <Typography color="text.secondary" variant='subtitle1' textAlign="center"> 404 </Typography>  
+                </Grid>
+                <Grid item xs={12}>
                     <Typography textAlign="center" sx={{fontSize: {xs: 40, sm: 50, md:60}, fontWeight: 700, color: "primary.main"}}> ¯\_(ツ)_/¯ </Typography>
                 </Grid>
                 <Grid item xs={12}>
