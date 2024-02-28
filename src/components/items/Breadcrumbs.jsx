0b69c80e8f1ac8a93ee422ddb278847cb9fe8e3d
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-02-27 22:37:20 -0600

    3.4.503 Update Breadcrumbs Design Items & 404 Error

--- src/components/items/Breadcrumbs.jsx
+++ src/components/items/Breadcrumbs.jsx
@@ -1,8 +1,9 @@
 // Breadcrumbs.js
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { Breadcrumbs, Typography, } from '@mui/material';
+import { Breadcrumbs, Typography, Chip} from '@mui/material';
 import { Container } from '../material-ui/Container';
+import { emphasize, styled } from '@mui/material/styles';
 import { NavigateNext, Home, Book, ContactPage, Warning, EmojiEmotions, Description, Subject, WorkHistory } from '@mui/icons-material';
 
 function Breadcrumb() {
@@ -12,22 +13,32 @@ function Breadcrumb() {
   return (
     <Container maxWidth="xl" sx={{py:1}}>
         <Breadcrumbs separator={<NavigateNext fontSize="small" />} aria-label="breadcrumb">
-            <Link to="/">
-                <Home sx={{mr: 0.5}} fontSize='inherit'/>
-                Home 
-            </Link>
+            <StyledBreadcrumb
+              component={Link}
+              to="/"
+              label="Home"
+              icon={<Home sx={{mr: 0.5}} fontSize='inherit'/>}
+            />
             {pathnames.map((name, index) => {
                 const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`;
                 const isLast = index === pathnames.length - 1;
                 const icon = getIcon(name);
                 return isLast ? (
-                    <Typography key={routeTo} color="text.primary">
-                        {icon} {name}
-                    </Typography>
+                    <StyledBreadcrumb
+                      component={Typography}
+                      key={routeTo}
+                      label={name}
+                      icon={icon}
+                      sx={{ '&:hover':{ cursor: "default" } }}
+                    />
                 ) : (
-                    <Link key={routeTo} to={routeTo}>
-                        {icon} {name}
-                    </Link>
+                    <StyledBreadcrumb
+                      component={Link}
+                      key={routeTo}
+                      to={routeTo}
+                      label={name}
+                      icon={icon}
+                    />
                 );
             })}
         </Breadcrumbs>
@@ -36,24 +47,45 @@ function Breadcrumb() {
 }
 
 function getIcon(name) {
-    if(name.includes('project-')){
-        return <Description sx={{mr: 0.5}} fontSize='inherit' />
-    }
-    if(!isNaN(name)){
-        return <Subject sx={{mr: 0.5}} fontSize='inherit' />
-    }
-    switch (name) {
-      case 'about-me':
-        return <EmojiEmotions sx={{mr: 0.5}} fontSize='inherit' />;
-      case 'contact':
-        return <ContactPage sx={{mr: 0.5}} fontSize='inherit' />;
-      case 'blog' :
-        return <Book sx={{mr: 0.5}} fontSize='inherrit' />
-      case 'projects' :
-        return <WorkHistory sx={{mr: 0.5}} fontSize='inherit' />
-      default:
-        return <Warning sx={{mr: 0.5}} fontSize='inherit'/>;
-    }
+  if(name.includes('project-')){
+    return <Description sx={{mr: 0.5}} fontSize='inherit' />
   }
+  if(!isNaN(name)){
+    return <Subject sx={{mr: 0.5}} fontSize='inherit' />
+  }
+  switch (name) {
+    case 'about-me':
+      return <EmojiEmotions sx={{mr: 0.5}} fontSize='inherit' />;
+    case 'contact':
+      return <ContactPage sx={{mr: 0.5}} fontSize='inherit' />;
+    case 'blog' :
+      return <Book sx={{mr: 0.5}} fontSize='inherrit' />
+    case 'projects' :
+      return <WorkHistory sx={{mr: 0.5}} fontSize='inherit' />
+    default:
+      return <Warning sx={{mr: 0.5}} fontSize='inherit'/>;
+  }
+}
+
+const StyledBreadcrumb = styled(Chip)(({ theme }) => {
+  const backgroundColor =
+    theme.palette.mode === "light"
+      ? theme.palette.grey[100]
+      : theme.palette.grey[800];
+  return {
+    backgroundColor,
+    height: theme.spacing(3),
+    color: theme.palette.text.primary,
+    fontWeight: theme.typography.fontWeightRegular,
+    "&:hover, &:focus": {
+      backgroundColor: emphasize(backgroundColor, 0.06),
+      cursor: "pointer"
+    },
+    "&:active": {
+      boxShadow: theme.shadows[1],
+      backgroundColor: emphasize(backgroundColor, 0.12),
+    },
+  };
+});
 
 export default Breadcrumb;
