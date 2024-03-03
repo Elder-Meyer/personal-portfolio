5fa6bdbf7d1cfa270b6f0c346ba1d23d09f0b63a
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-03-02 22:23:15 -0600

    3.4.604 Create ChipDownloadCV.jsx

---
+++ src/views/about-me/ChipDownloadCV.jsx
@@ -0,0 +1,26 @@
+import { FileDownloadOutlined } from "@mui/icons-material";
+import { Chip } from "../../components/material-ui/Chip";
+import CVElderMeyer from "../../assets/docs/CV-ElderMeyer.pdf"
+
+export const ChipDownloadCV = () => {
+  return (
+    <Chip
+        onClick={handleChipClick}
+        label="Descargar"
+        icon={<FileDownloadOutlined/>}
+    />
+  )
+}
+
+
+function handleChipClick() {
+    const link = document.createElement('a');
+    link.href = CVElderMeyer;
+    link.target = '_blank';
+    link.download = 'CV-ElderMeyer';
+    // Simular un clic en el enlace para iniciar la descarga
+    document.body.appendChild(link);
+    link.click();
+    // Limpiar el elemento de enlace después de la descarga
+    document.body.removeChild(link);
+  }
