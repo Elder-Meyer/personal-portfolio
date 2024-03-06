29244b323e57a8b7eba200fa26e28eb735888529
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-03-06 01:16:35 -0600

    3.5.101 Update WorkingOnThat component with logo img

--- src/components/items/WorkingOnThat.jsx
+++ src/components/items/WorkingOnThat.jsx
@@ -1,15 +1,17 @@
 import { Typography } from '../material-ui/Typography'
 import { Grid } from '../material-ui/Grid'
 import { useTranslation } from 'react-i18next';
+import { CubeGD } from './CubeGD';
+import { Stack } from '../material-ui/Stack';
 
 export const WorkingOnThat = () => {
     const { t } = useTranslation();
     return (
         <Grid container spacing={2} py={5}>
             <Grid item xs={12}>
-                <Typography textAlign="center" sx={{fontSize: {xs: 40, sm: 50, md:60}, fontWeight: 700, color: "primary.main"}}>
-                    {"(◕‿↼)"}
-                </Typography>
+                <Stack width="100%" justifyContent="center" direction="row">
+                    <CubeGD animate={2} large={100}/>
+                </Stack>
             </Grid>
             <Grid item xs={12}>
                 <Typography textAlign="center" sx={{fontSize: {xs: 40, sm: 50, md:60}, fontWeight: 700, color: "primary.main"}}>
