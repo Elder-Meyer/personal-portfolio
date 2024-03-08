53ebc8ad06804ea78fcdb24d47636bc3bb0afcdb
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-03-08 03:52:35 -0600

    3.6.101 Test Solve Error norunbuild in Vercel

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,6 +1,6 @@
 import { lazy } from 'react'
 const Presentation = lazy(() => import('./Presentation/Presentation').then((module) => ({ default:module.Presentation })))
-const Skills = lazy(() => import('./Skills/Skills').then((module) => ({ default:module.Skills })))
+const Skills = lazy(() => import('./skills/Skills').then((module) => ({ default:module.Skills })))
 const Projects = lazy(() => import('../projects/Projects').then((module) => ({ default:module.Projects })))
 import { Box } from '../../components/material-ui/Box'
 import { Divider } from '../../components/material-ui/Divider'
