202da486743e9beef4c058b7f316e355c6e11cbd
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-03-10 03:44:14 -0600

    3.602.3 Add animation icon ThemeSwitcher.jsx

--- src/components/items/ThemeSwitcher.jsx
+++ src/components/items/ThemeSwitcher.jsx
@@ -6,22 +6,32 @@ import { Tooltip } from '../material-ui/Tooltip';
 
 const ThemeSwitcher = ({ darkMode, handleChangeTheme }) => {
   const { t } = useTranslation()
-
+  const [isRotated, setIsRotated] = useState(false);
   const [hovered, setHovered] = useState(false);
   const isMobile = useMediaQuery('(max-width:899px)');
 
+  const handleIconClick = () => {
+    setIsRotated(!isRotated);
+    handleChangeTheme(); // Cambiar tema
+  };
+
   return (
     <Tooltip title={darkMode ? t("navbar.darkThemeText") : t("navbar.lightThemeText") } placement={isMobile ? "top" : "bottom"} arrow>
-      <IconButton onClick={handleChangeTheme}
-        sx={{ border: 1, borderColor: 'divider', color : "inherit" }}
+      <IconButton onClick={handleIconClick}
+        sx={{ 
+          color : "inherit",
+          transition: 'transform 0.5s ease',
+          transform: isRotated ? 'rotate(360deg)' : 'rotate(180deg)',
+        }}
         onMouseEnter={() => setHovered(true)}
         onMouseLeave={() => setHovered(false)}
         aria-label={darkMode ? t("navbar.darkThemeText") : t("navbar.lightThemeText") }
+        size='large'
       >
         {darkMode ? (
-          hovered ? <LightMode /> : <LightModeOutlined />
+          hovered ? <LightMode fontSize='inherit' /> : <LightModeOutlined fontSize='inherit' />
         ) : (
-          hovered ? <DarkMode /> : <DarkModeOutlined />
+          hovered ? <DarkMode fontSize='inherit' /> : <DarkModeOutlined fontSize='inherit' />
         )}
       </IconButton>
     </Tooltip>
