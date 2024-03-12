355584e8edf7c6e9c3e2184523310675ed36fa49
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-03-12 00:35:45 -0600

    3.701.0 Update ProjectDetails.jsx background based on principal image

--- src/views/projects/ProjectDetails.jsx
+++ src/views/projects/ProjectDetails.jsx
@@ -15,6 +15,7 @@ import { Paper } from "../../components/material-ui/Paper";
 import { Stack } from "../../components/material-ui/Stack";
 import { Typography } from "../../components/material-ui/Typography";
 import { Loader } from "../../components/items/Loader";
+import { Box } from "../../components/material-ui/Box";
 
 export const ProjectDetails = () => {
   const { t } = useTranslation();
@@ -42,109 +43,112 @@ export const ProjectDetails = () => {
 
   if (!translatedProject){ return <Loader/> }
   return (
-    <Container maxWidth="xl" sx={{minHeight: {xs: "55vh", sm: "60vh", md: "67vh"}, display: "flex", alignItems: "center" }}>
-      <Paper elevation={0} sx={{p: {xs:1, md:2, lg:3}, borderRadius: 5}}>
-        <Grid container spacing={2}>
-          {/* TITULO */}
-          <Grid item xs={12}>
-            <Stack direction={{xs:"column", sm: "row"}} spacing={0} display="flex" alignItems="flex-start">
-              <IconButton aria-label="back" component={LinkRoute} to="/projects">
-                <ArrowBackSharp />
-              </IconButton>
-              <Typography color="primary.light" variant="h4" fontWeight={800}>
-                {translatedProject.title}
+    <>
+      <Box component="img" src={translatedProject.imgPrincipal} sx={{height: "40%", width: "100%", filter: "blur(100px)", opacity: 0.5, position: "absolute", display: {xs: "block", sm: "block", md: "none"}}} />
+      <Container maxWidth="xl" sx={{minHeight: {xs: "55vh", sm: "60vh", md: "67vh"}, display: "flex", alignItems: "center", position: "relative", zIndex: 10 }}>
+        <Paper elevation={0} sx={{p: {xs:1, md:2, lg:3}, borderRadius: 5}}>
+          <Grid container spacing={2}>
+            {/* TITULO */}
+            <Grid item xs={12}>
+              <Stack direction={{xs:"column", sm: "row"}} spacing={0} display="flex" alignItems="flex-start">
+                <IconButton aria-label="back" component={LinkRoute} to="/projects">
+                  <ArrowBackSharp />
+                </IconButton>
+                <Typography color="primary.light" variant="h4" fontWeight={800}>
+                  {translatedProject.title}
+                </Typography>
+              </Stack>
+              <Divider />
+            </Grid>
+
+            {/* SUBTITULO */}
+            <Grid item xs={12}>
+              <Typography color="text.primary" variant="body1">
+                {translatedProject.shortDescription}
               </Typography>
-            </Stack>
-            <Divider />
-          </Grid>
-
-          {/* SUBTITULO */}
-          <Grid item xs={12}>
-            <Typography color="text.primary" variant="body1">
-              {translatedProject.shortDescription}
-            </Typography>
-          </Grid>
-
-          {/* GRID - PRINCIPAL DATA */}
-          <Grid container item spacing={2}>
-            {/* IMAGE CONTAINER */}
-            <Grid container item xs={12} sm={12} md={6} lg={7} xl={8}
-              component={Stack}
-              direction="row"
-              justifyContent="center"
-              alignItems="center"
-              sx={{aspectRatio: "384/199"}}
-            >
-              <Card elevation={3}>
-                <CardMedia component="img" width="954.66px" height="494.72px"
-                  image={translatedProject.imgPrincipal}
-                  alt={translatedProject.id}
-                  sx={{aspectRatio: "384/199", height: "auto", width: "100%" }}
-                />
-              </Card>
             </Grid>
-            {/* PRINCIPAL DATA CONTAINER */}
-            <Grid container item xs={12} sm={12} md={6} lg={5} xl={4}>
-              {/* ABOUT - INFO */}
-              <Grid item xs={12}>
-                <Stack spacing={1}>
-                  <Typography color="text.secondary" variant="caption" display="flex" justifyContent="flex-end">
-                    {translatedProject.date}
-                  </Typography>
-                  <Divider />
-                  <Typography color="primary.light" variant="h5" fontWeight={400}>
-                    {t('projectsdetails.about')}
-                  </Typography>
-                  { translatedProject.largeDescription.split('\n').map((paragraph, index) => (
-                    <Typography key={index} color="text.primary" variant="body1" gutterBottom>
-                      {paragraph}
+
+            {/* GRID - PRINCIPAL DATA */}
+            <Grid container item spacing={2}>
+              {/* IMAGE CONTAINER */}
+              <Grid container item xs={12} sm={12} md={6} lg={7} xl={8}
+                component={Stack}
+                direction="row"
+                justifyContent="center"
+                alignItems="center"
+                sx={{aspectRatio: "384/199"}}
+              >
+                <Card elevation={3}>
+                  <CardMedia component="img" width="954.66px" height="494.72px"
+                    image={translatedProject.imgPrincipal}
+                    alt={translatedProject.id}
+                    sx={{aspectRatio: "384/199", height: "auto", width: "100%" }}
+                  />
+                </Card>
+              </Grid>
+              {/* PRINCIPAL DATA CONTAINER */}
+              <Grid container item xs={12} sm={12} md={6} lg={5} xl={4}>
+                {/* ABOUT - INFO */}
+                <Grid item xs={12}>
+                  <Stack spacing={1}>
+                    <Typography color="text.secondary" variant="caption" display="flex" justifyContent="flex-end">
+                      {translatedProject.date}
                     </Typography>
-                  ))}
-                </Stack>
-              </Grid>
-              {/* TECHNOLOGIES DATA CONTAINER */}
-              <Grid item xs={12} component={Stack}>
-                <Stack spacing={1} justifyContent={"flex-end"}>
-                  <Typography color="primary.light" variant="h5">
-                    {t('projectsdetails.technologies')}
-                  </Typography>
-                  <Stack direction="row" flexWrap="wrap" gap={0.5}>
-                    { translatedProject && translatedProject.techs 
-                      ? translatedProject.techs.map((tech, index) => (
-                        <Chip key={index}
-                          label={tech}
-                          variant="filled" 
-                        />
-                      ))
-                      : "No existe la propiedad techs"
-                    }
+                    <Divider />
+                    <Typography color="primary.light" variant="h5" fontWeight={400}>
+                      {t('projectsdetails.about')}
+                    </Typography>
+                    { translatedProject.largeDescription.split('\n').map((paragraph, index) => (
+                      <Typography key={index} color="text.primary" variant="body1" gutterBottom>
+                        {paragraph}
+                      </Typography>
+                    ))}
                   </Stack>
-                </Stack>
+                </Grid>
+                {/* TECHNOLOGIES DATA CONTAINER */}
+                <Grid item xs={12} component={Stack}>
+                  <Stack spacing={1} justifyContent={"flex-end"}>
+                    <Typography color="primary.light" variant="h5">
+                      {t('projectsdetails.technologies')}
+                    </Typography>
+                    <Stack direction="row" flexWrap="wrap" gap={0.5}>
+                      { translatedProject && translatedProject.techs 
+                        ? translatedProject.techs.map((tech, index) => (
+                          <Chip key={index}
+                            label={tech}
+                            variant="filled" 
+                          />
+                        ))
+                        : "No existe la propiedad techs"
+                      }
+                    </Stack>
+                  </Stack>
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
 
-          {/* WEBSITE */}
-          <Grid item xs={12} sm={6}>
-            <Stack direction="row" spacing={1} alignItems="center">
-              <Language color="primary"/>
-              <Link color="primary" variant="h5" href={translatedProject.href} target="_BLANK" underline="always">
-                {t('projectsdetails.website')}
-              </Link>
-            </Stack>
-          </Grid>
+            {/* WEBSITE */}
+            <Grid item xs={12} sm={6}>
+              <Stack direction="row" spacing={1} alignItems="center">
+                <Language color="primary"/>
+                <Link color="primary" variant="h5" href={translatedProject.href} target="_BLANK" underline="always">
+                  {t('projectsdetails.website')}
+                </Link>
+              </Stack>
+            </Grid>
 
-          {/* GITHUB */}
-          <Grid item xs={12} sm={6}>
-            <Stack direction="row" spacing={1} alignItems="center">
-              <GitHub color="primary"/>
-              <Link color="primary" variant="h5" href={translatedProject.repo} target="_BLANK" underline="always">
-                {t('projectsdetails.repo')}
-              </Link>
-            </Stack>
+            {/* GITHUB */}
+            <Grid item xs={12} sm={6}>
+              <Stack direction="row" spacing={1} alignItems="center">
+                <GitHub color="primary"/>
+                <Link color="primary" variant="h5" href={translatedProject.repo} target="_BLANK" underline="always">
+                  {t('projectsdetails.repo')}
+                </Link>
+              </Stack>
+            </Grid>
           </Grid>
-        </Grid>
-      </Paper>          
-    </Container>
+        </Paper>          
+      </Container>
+    </>
   );
 };
