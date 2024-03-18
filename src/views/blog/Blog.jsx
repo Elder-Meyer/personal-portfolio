aa64b4870a44369745980e31e87a7df2b5e02349
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-03-18 04:27:20 -0600

    3.704.0 Update Structure responsive item Blog.jsx

--- src/views/blog/Blog.jsx
+++ src/views/blog/Blog.jsx
@@ -2,15 +2,13 @@ import { useTranslation } from 'react-i18next'
 import { Container } from '../../components/material-ui/Container'
 import { Grid } from '../../components/material-ui/Grid'
 import { Typography } from '../../components/material-ui/Typography'
-import { WorkingOnThat } from '../../components/items/WorkingOnThat'
-import { Paper } from '../../components/material-ui/Paper'
 import { CardContent } from '../../components/material-ui/CardContent'
 import { CardActions } from '../../components/material-ui/CardActions'
 import { Button } from '../../components/material-ui/Button'
 import { Box } from '../../components/material-ui/Box'
 import { Card } from '../../components/material-ui/Card'
-import { Chip } from '../../components/material-ui/Chip'
 import { CardMedia } from '../../components/material-ui/CardMedia'
+import { Link } from "react-router-dom"
 // import { useEffect, useState } from 'react'
 // import { getItems } from '../../app/api'
 
@@ -31,101 +29,48 @@ export const Blog = () => {
     <Container maxWidth="lg" sx={{minHeight: {xs: "55vh", sm: "60vh", md: "67vh"}, display: "flex", alignItems: "flex-start" }}>
       <Grid container spacing={2} py={1}>
         <Grid item xs={12}>
-          <Typography color="text.secondary" variant='subtitle1' textAlign="left"> Articulos </Typography>  
+          <Typography color="text.secondary" variant='subtitle1' textAlign="left"> Blog </Typography>  
         </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Card variant='outlined'>
+        {/* reserva */}        
+        <Grid item xs={12} sm={12} md={4}>
+          <Card sx={{ display: 'flex', flexDirection: {xs: "column", sm: "row", md: "column"} }} variant="outlined">
             <CardMedia
-              sx={{ height: 194 }}  
-              // component="img"
-              // height="194"
-              image="https://mui.com/static/images/cards/paella.jpg"
-              alt="Paella dish"
+              component="img"
+              sx={{ 
+                width: { xs: "auto", sm: 200, md: "auto"},
+                height: { xs: 194, sm: "auto", md: 194 }
+              }}
+              image="https://www.oxfordwebstudio.com/user/pages/06.da-li-znate/sta-je-css/sta-je-css.png"
+              alt="Live from space album cover"
             />
-            <CardContent sx={{p:3}}>
-              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                26 October 2017
-              </Typography>
-              <Typography variant="h5" component="div" gutterBottom>
-                Header style Collector for your prototypes
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                CSS
-              </Typography>
-              <Typography variant="body2">
-              It´s very easy to create stylish and beautiful prototypes for your futurre proyects both graphical and dynamic
-                <br />
-                {'"a benevolent smile"'}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small" variant="text" disableElevation>Read More</Button>
-            </CardActions>
+            <Box sx={{ display: 'flex', flexDirection: 'column', width: "100%" }}>
+              <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                  18 March 2024
+                </Typography>
+                <Typography variant="h5" component="div" gutterBottom>
+                  How center an element with CSS
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  CSS
+                </Typography>
+                <Typography variant="body2">
+                It´s very easy to create stylish and beautiful prototypes for your futurre proyects both graphical and dynamic
+                  <br />
+                  {'"a benevolent smile"'}
+                </Typography>
+              </CardContent>
+              <CardActions >
+                <Button size="small" variant="contained" fullWidth disableElevation
+                  component={Link}
+                  to={"post-01-centrar-un-div"}
+                >
+                  Read More
+                </Button>
+              </CardActions>
+            </Box>
           </Card>
         </Grid>
-        
-        <Grid item xs={12} sm={6} md={4}>
-          <Card variant='outlined'>
-            <CardMedia
-              sx={{ height: 194 }}  
-              // component="img"
-              // height="194"
-              image="https://mui.com/static/images/cards/live-from-space.jpg"
-              alt="Paella dish"
-            />
-            <CardContent sx={{p:3}}>
-              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                26 October 2017
-              </Typography>
-              <Typography variant="h5" component="div" gutterBottom>
-                Header style Collector for your prototypes
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                CSS
-              </Typography>
-              <Typography variant="body2">
-              It´s very easy to create stylish and beautiful prototypes for your futurre proyects both graphical and dynamic
-                <br />
-                {'"a benevolent smile"'}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small" variant="text" disableElevation>Read More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        
-        <Grid item xs={12} sm={6} md={4}>
-          <Card variant='outlined'>
-            <CardMedia
-              sx={{ height: 194 }}  
-              // component="img"
-              // height="194"
-              image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
-              alt="Paella dish"
-            />
-            <CardContent sx={{p:3}}>
-              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                26 October 2017
-              </Typography>
-              <Typography variant="h5" component="div" gutterBottom>
-                Header style Collector for your prototypes
-              </Typography>
-              <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                CSS
-              </Typography>
-              <Typography variant="body2">
-              It´s very easy to create stylish and beautiful prototypes for your futurre proyects both graphical and dynamic
-                <br />
-                {'"a benevolent smile"'}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small" variant="text" disableElevation>Read More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        
         
         
       </Grid>
