2ad383e2726e13e135ed992db9af6a57ba955fa2
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-03-23 18:23:11 -0600

    3.802.5 Improve Structure CookiesDialog.jsx

--- src/components/items/CookiesDialog.jsx
+++ src/components/items/CookiesDialog.jsx
@@ -13,59 +13,39 @@ export const CookiesDialog = () => {
     const [bannerOpen, setBannerOpen] = useState(true);
 
     const closeBanner = () => {
-        setBannerOpen(false);
-        Cookies.set('cookieBannerClosed', 'true', { expires: 365 }); 
+      setBannerOpen(false);
+      Cookies.set('cookieBannerClosed', 'true', { expires: 365 }); 
     };
 
     useEffect(() => {
-        const isBannerClosed = Cookies.get('cookieBannerClosed'); 
-        if (isBannerClosed === 'true') {
-            setBannerOpen(false); 
-        }
+      const isBannerClosed = Cookies.get('cookieBannerClosed'); 
+      if (isBannerClosed === 'true') { setBannerOpen(false) }
     }, []);
 
   return (
     <TrapFocus open={bannerOpen} disableAutoFocus disableEnforceFocus>
-        <Slide direction="up" appear={false} in={bannerOpen}>
-          <Paper
-            elevation={4}
-            role="dialog"
-            aria-modal="false"
-            aria-label="Cookie banner"
-            tabIndex={-1}
-            sx={{
-              position: 'fixed',
-              width: { sm : "auto", md: 526},
-              bottom: { xs: 0, sm: 0, md:30},
-              right: { xs: 0, sm: 0, md:30},
-              left: { xs: 0, sm: 0, md: "auto" },
-              m: {xs: 1, sm: 2, md: 3},
-              p: 3,
-              borderWidth: 2,
-              borderColor: "divider",
-              zIndex :100,
-            }}
-          >
-            <Stack
-              direction={"column"}
-              justifyContent="center"
-              gap={2}
-            >
-              <Box>
-                <CookieOutlined fontSize="large" sx={{mb:1}}/>
-                <Typography fontWeight="bold">This website uses cookies</Typography>
-                <Typography variant="body2">
-                  To improve your experience this website use cookies
-                </Typography>
-              </Box>
-              
-              <Button size="small" onClick={closeBanner} variant="contained">
-                Allow all
-              </Button>
-              
-            </Stack>
-          </Paper>
-        </Slide>
+      <Slide direction="up" appear={false} in={bannerOpen}>
+        <Paper variant="outlined" role="dialog" aria-modal="false" aria-label="Cookie banner" tabIndex={-1}
+          sx={{
+            position: 'fixed',
+            width: { sm : "auto", md: 526},
+            bottom: { xs: 0, sm: 0, md:30},
+            right: { xs: 0, sm: 0, md:30},
+            left: { xs: 0, sm: 0, md: "auto" },
+            m: {xs: 1, sm: 2, md: 3},
+            p: 3, borderWidth: 2, borderColor: "divider", zIndex :100,
+          }}
+        >
+          <Stack direction={"column"} justifyContent="center" gap={2}>
+            <Box>
+              <CookieOutlined fontSize="large" sx={{mb:1}}/>
+              <Typography fontWeight="bold">This website uses cookies</Typography>
+              <Typography variant="body2">To improve your experience this website use cookies</Typography>
+            </Box>
+            <Button size="small" onClick={closeBanner} variant="contained">Allow all</Button>
+          </Stack>
+        </Paper>
+      </Slide>
     </TrapFocus>
   )
 }
