4b5d6038c53fb46745b4a61d64c12321b628dd5c
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-04-08 21:07:51 -0600

    3.10.210 Update projects section minHeight in Projects view

--- src/views/projects/Projects.jsx
+++ src/views/projects/Projects.jsx
@@ -4,11 +4,14 @@ import { Container } from '../../components/material-ui/Container';
 import { Stack } from '../../components/material-ui/Stack';
 import { Typography } from '../../components/material-ui/Typography';
 import { Web } from './Web';
+import { useLocation } from 'react-router-dom';
 
 export const Projects = () => {
   const { t } = useTranslation();
+  const location = useLocation()
+  const isProjects = location.pathname === "/projects";
   return (
-    <Box component='section' sx={{ px:{ xs: 0, sm:1, md:2, lg:2}, py: {xs:3, md:10, xl: 20} }}>
+    <Box component='section' sx={{ px:{ xs: 0, sm:1, md:2, lg:2}, py: isProjects ? 1 : {xs:3, md:10, xl: 20}, minHeight: isProjects ?  {xs: "auto", sm: "auto", md: "62vh"} : "auto" }} >
       <Container maxWidth="xl">
         <Stack direction="column" spacing={1} pb={2}>
           <Typography color="primary.main" textAlign="center" sx={{fontSize: 40, fontWeight: 500}} >
