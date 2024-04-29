e96cb27ecfdc44971a97d6890d0788345adf68aa
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-04-28 22:28:30 -0600

    3.14.402 Change chipDownload for button - About-me

--- src/views/about-me/ChipDownloadCV.jsx
+++ src/views/about-me/ChipDownloadCV.jsx
@@ -1,16 +1,20 @@
 import { FileDownloadOutlined } from "@mui/icons-material";
-import { Chip } from "@mui/material";
+import { Button, Chip } from "@mui/material";
 import CVElderMeyer from "../../assets/docs/CV-ElderMeyer.pdf"
 import { useTranslation } from "react-i18next";
 
 export const ChipDownloadCV = () => {
   const { t } = useTranslation()
   return (
-    <Chip
+    <Button
       onClick={handleChipClick}
-      label={t("aboutMe.downloadCv")}
-      icon={<FileDownloadOutlined/>}
-    />
+      endIcon={<FileDownloadOutlined/>}
+      variant="contained"
+      sx={{borderRadius: 5}}
+      disableElevation
+    >
+      {t("aboutMe.downloadCv")}
+    </Button>
   )
 }
 
