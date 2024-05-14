48d79d775330009b7bba7f7de22a3a874dd9bc1a
Elder-Meyer <meyer.sanchez3000@gmail.com>
2024-05-13 21:06:21 -0600

    3.14.408 Update color style subtitle caption text form contact

--- src/views/contact/FormContact.jsx
+++ src/views/contact/FormContact.jsx
@@ -18,11 +18,11 @@ export const FormContact = () => {
   return (
     <Paper sx={{ p: {xs:1, sm:3, md:5}, mx: {xs: 1, sm: 2, md: 10, lg: 20, xl: 50 }, my: 2}}  elevation={0}>
       <Typography variant="h4" color="primary" textAlign="center"> {t("contact.title")} </Typography>
-      <Typography variant="body1" color="primary.light" textAlign="center" gutterBottom> {t("contact.subtitle")} </Typography>
+      <Typography variant="body1" color="text.secondary" textAlign="center" gutterBottom> {t("contact.subtitle")} </Typography>
       <Divider variant="fullwidth">
         <Chip size="small" label="Or" color="primary" variant="filled" />
       </Divider>
-      <Stack component={Typography} textAlign="center" variant="subtitle2" paragraph direction="row" alignItems="center" justifyContent="center" spacing={0.5}>
+      <Stack component={Typography} color="text.secondary" textAlign="center" variant="subtitle2" paragraph direction="row" alignItems="center" justifyContent="center" spacing={0.5}>
         <div>Check more info {" "}</div>
         <Link color="text.primary" underline='always' component={LinkRoute} to='/about-me'> 
           {"About Me "}
